6dc5f6b7dbe9c78190f10eb70de7e361cf3ea604
dontknowhy <3365304317@qq.com>
2026-07-28 17:34:54 +0800

    复制了一份fox_certus.mk，CI貌似只认twrp后缀

---
+++ twrp_certus.mk
@@ -0,0 +1,19 @@
+#
+# Copyright (C) 2023 The Android Open Source Project
+# Copyright (C) 2023 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
+
+# Inherit some common AOSP stuff.
+#$(call inherit-product, vendor/twrp/config/common.mk)
+
+PRODUCT_DEVICE := certus
+PRODUCT_NAME := fox_certus
+PRODUCT_BRAND := xiaomi
+PRODUCT_MODEL := Redmi 6/6A
+PRODUCT_MANUFACTURER := xiaomi
